4b596db387cab79398fb3e4a8abe2a3f07673d92
TrovaBruno <bruno.01ceni@gmail.com>
2024-12-27 22:51:31 -0300

    Adicionar aulas 59 a 64

---
+++ aula60.py
@@ -0,0 +1,7 @@
+# Operação ternária (condicional de uma linha)
+# <valor> if <condicao> else <outro valor>
+
+condicao = 10 == 10
+variavel = 'Está certo' if condicao else 'Errado'
+print(variavel)
+
