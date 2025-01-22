86e1cbe9ab5814686aa10c3e94907cc826e94415
TrovaBruno <bruno.01ceni@gmail.com>
2025-01-21 23:11:12 -0300

    aula 98 até 100 - tem package

---
+++ aula99.py
@@ -0,0 +1,8 @@
+from sys import path
+from aula99_package.modulo import soma_do_modulo
+from aula99_package.modulo import *
+
+
+print(soma_do_modulo(1,2))
+print(variavel)
+# print(*path, sep='n')
---
+++ aula99pt2.py
@@ -0,0 +1,7 @@
+# from aula99_package.modulo import fala_oi, soma_do_modulo
+
+# print(__name__)
+# fala_oi()
+
+import aula99_package
+print(aula99_package.soma_do_modulo(2,3))
